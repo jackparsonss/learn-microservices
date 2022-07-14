1e0c5ce59a7e8bfd8ad7450bec6d38dc67929eec
Jack Parsons <jackpar1113@gmail.com>
2022-07-13 20:21:27 -0600

    complete mini app

--- miniapp/posts/index.js
+++ miniapp/posts/index.js
@@ -14,7 +14,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const title = req.body.title;
 
@@ -23,7 +23,7 @@ app.post("/posts", async (req, res) => {
     title,
   };
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: posts[id],
   });
@@ -37,5 +37,6 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log("NEW VERSION");
   console.log("Listening at http://localhost:4000");
 });
